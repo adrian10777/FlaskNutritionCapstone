368ec9aef45dd3b6eae1620fa41c3c5c5f21f995
Adrian Henriquez <adrian10777@hotmail.com>
2022-02-13 13:17:37 -0500

    newcommit

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_cors import CORS
 
 from .api.routes2 import api
+from .payments.routesstripe import payments
 # import blueprints
 from flask_sqlalchemy import SQLAlchemy
 
@@ -15,6 +16,7 @@ CORS(app, origins=['*'])
 app.config.from_object(Config)
 
 app.register_blueprint(api)
+app.register_blueprint(payments)
 
 db.init_app(app)
 migrate = Migrate(app, db)
